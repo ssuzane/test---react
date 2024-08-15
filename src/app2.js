a3773f22847fea0f9bb86908ec69673ec9968ad6
SUZANE ARCANJO DA HORA <23111828@CFP127ALUNO.LOCAL>
2024-08-15 17:21:46 -0300

    react

---
+++ src/app2.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App2.css';
+
+function App2() {
+  return (
+  <div>
+        <input className='input' type='text'/>
+        <button>Adicionar</button>
+
+      </div>
+  );
+}
+
+export default App2;
